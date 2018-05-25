e049346eee3c8af654659a6076f93be627b26d49
Matthew Penza <mpenza@cs.princeton.edu>
2018-05-25 15:49:49 -0400

    neatened clean.py

--- src/clean.py
+++ src/clean.py
@@ -1,38 +1,120 @@
 # -*- coding: utf-8 -*-
-import sys
+import sys, re
 reload(sys)
 sys.setdefaultencoding('utf-8')
 
+basic_replacements = {
+    '0': '',
+    '1': '',
+    '2': '',
+    '3': '',
+    '4': '',
+    '5': '',
+    '6': '',
+    '7': '',
+    '8': '',
+    '9': '',
+    
+    'ä': 'a',
+    'Ä': 'A',
+    'á': 'a',
+    'Á': 'A',
+    'æ': 'ae',
+    'ǽ': 'ae',
+    'Æ': 'Ae',
+    'Ǽ': 'Ae',
+
+    'ë': 'e',
+    'Ë': 'E',
+    'é': 'e',
+    'É': 'E',
+
+    'ï': 'i',
+    'Ï': 'I',
+    'í': 'i',
+    'Í': 'I',
+
+    'ö': 'o',
+    'Ö': 'O',
+    'ó': 'o',
+    'Ó': 'O',
+    'œ': 'oe',
+    'Œ': 'Oe',
+
+    'ü': 'u',
+    'Ü': 'U',
+    'ú': 'u',
+    'Ú': 'U',
+
+    'ÿ': 'y',
+    'Ÿ': 'Y',
+    'ý': 'y',
+    'Ý': 'Y',
+    
+    'j': 'i',
+    'J': 'I',
+
+    '[': '',
+    ']': '',
+    '(': '',
+    ')': '',
+    '\'': '',
+    '`': '',
+
+    '  ': ' ',
+    '\n ': '\n',
+    ' \n': '\n',
+    '\n\n': '\n'
+}
+
+newpunct = {
+    ',': ' , ',
+    '.': ' . ',
+    '?': ' ? ',
+    '!': ' ! ',
+    ':': ' : ',
+    ';': ' ; ',
+    '–': ' – ',
+    '—': ' — ',
+    '-': ' - '
+}
+
+macron_replacements = {
+    'ā': 'a',
+    'ē': 'e',
+    'ī': 'i',
+    'ō': 'o',
+    'ū': 'u',
+    'ȳ': 'y', # Unicode code 563
+    'ӯ': 'y', # Unicode code 1263
+    'Ā': 'A',
+    'Ē': 'E',
+    'Ī': 'I',
+    'Ō': 'O',
+    'Ū': 'U',
+    'Ȳ': 'Y'
+}
+
+def multiple_replace(text, adict):
+    """ Adapted from 'Python Cookbook' 2nd Ed.: 1.18. Replacing Multiple Patterns in a Single Pass """
+    rx = re.compile('|'.join(map(re.escape, adict)))
+    def one_xlat(match):
+        return adict[match.group(0)]
+    return rx.sub(one_xlat, text)
+
 def clean_text(txt):
-    chars = [t.replace('[', '').replace(']', '').replace('(', '').replace(')', '').replace('\'', '').replace('`', '') for t in txt if not t.isdigit()]
-    text = ""
-    for c in chars: text = text + c
-    text = text.replace('æ', 'ae').replace('œ', 'oe').replace('ǽ', 'ae').replace('Æ', 'Ae').replace('Ǽ', 'Ae').replace('Œ', 'Oe').replace('j', 'i').replace('J', 'I')
-    text = text.replace('ä', 'a').replace('Ä', 'A').replace('ë', 'e').replace('Ë', 'E').replace('ï', 'i').replace('Ï', 'I').replace('ö', 'o').replace('Ö', "O").replace('ü', 'u').replace('Ü', 'U').replace('ÿ', 'y').replace('Ÿ', 'Y')
-    text = text.replace('á', 'a').replace('é', 'e').replace('í', 'i').replace('ó', 'o').replace('ú', 'u').replace('ý', 'y')
-    while '  ' in text: text = text.replace('  ', ' ')
-    while '\n ' in text: text = text.replace('\n ', '\n')
-    while ' \n' in text: text = text.replace(' \n', '\n')
-    while '\n\n' in text: text = text.replace('\n\n', '\n')
-    text = text.replace('\n', ' \n ')
-
-
-    return text
+    return multiple_replace(txt, basic_replacements).replace('\n', ' \n ')
 
 def clean_lines(txt):
     txt = clean_text(txt)
     text = ""
     for line in txt.split('\n'):
-        line = line.strip().replace(',', ' , ').replace('.', ' . ').replace('?', ' ? ').replace('!', ' ! ').replace(':', ' : ').replace(';', ' ; ').replace('–', ' – ').replace('—', ' — ').replace('-', ' - ')
-        text += line + '\n'
+        text += multiple_replace(line.strip(), newpunct) + '\n'
 
     return text
 
 def demacronized_lines(txt):
-    mac = clean_lines(txt)
-    demac = mac.replace('ā', 'a').replace('ē', 'e').replace('ī', 'i').replace('ō', 'o').replace('ū', 'u').replace('ȳ', 'y').replace('ӯ'.decode('utf-8'), 'y')
-    demac = demac.replace('Ā', 'A').replace('Ē', 'E').replace('Ī', 'I').replace('Ō', 'O').replace('Ū', 'U').replace('Ȳ', 'Y')
-    return demac
+    return multiple_replace(clean_lines(txt), macron_replacements)
 
 def newline_locs(txt):
     locs = []
@@ -41,15 +123,31 @@ def newline_locs(txt):
     return locs
 
 def main():
-    with open("john.txt", 'r') as f: txt = f.read()
+    print '#### BEGIN TEXT-CLEANING TESTS ##################################'
     
+    with open("../input/aeneid_naturalized.txt", 'r') as f: txt = f.read()
+    
+    print '#### ORIGINAL TEXT ##############################################'
     print txt
-    print '---------------------------------------------------------------'
+    print '#################################################################\n'
+
+    print '#### BASIC TEXT-CLEANING ########################################'
     cleantxt = clean_text(txt)
     print cleantxt
-    print newline_locs(cleantxt)
-    print '---------------------------------------------------------------'
-    print clean_lines(txt)
-    print '---------------------------------------------------------------'
+    print '#################################################################\n'
 
-#main()
+    print '#### LINEBREAK LOCATIONS ########################################'
+    print newline_locs(cleantxt)
+    print '#################################################################\n'
+
+
+    print '#### BASIC TEXT-CLEANING WITH LINE-CLEANING #####################'
+    print clean_lines(txt)
+    
+    print '#### TEXT DEMACRONIZATION #######################################'
+    print demacronized_lines(txt)
+    print '#################################################################\n'
+
+    print '#### END TEXT-CLEANING TESTS ####################################'
+
+main()
